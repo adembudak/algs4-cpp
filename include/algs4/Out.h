724635ed753c31ab365b167d13953aa18b162cee
vanilla milkshake <vanillamilkshakepls@gmail.com>
2021-12-01 03:08:53 +0300

    Ported: Out
    Out class can leave output to console or file

---
+++ include/algs4/Out.h
@@ -0,0 +1,48 @@
+#ifndef OUT_H
+#define OUT_H
+
+#include <iosfwd>
+#include <iostream>
+
+#include <cstdint>
+
+namespace algs4 {
+using byte = std::uint8_t;
+
+class Out {
+  private:
+    std::ostream &out;
+
+  public:
+    Out(std::ofstream &os);
+    Out(std::ostream &os = std::cout);
+
+    void close();
+
+    void println();
+    // void println(Object x) // not implmented
+    void println(const bool x);
+    void println(const char x);
+    void println(const double x);
+    void println(const float x);
+    void println(const int x);
+    void println(const long x);
+    void println(const byte x);
+
+    void print();
+    // void print(Object x) // not implmented
+    void print(const bool x);
+    void print(const char x);
+    void print(const double x);
+    void print(const float x);
+    void print(const int x);
+    void print(const long x);
+    void print(const byte x);
+
+    // TODO: implement formatted output to file
+    // void printf(const char *format, ...);
+};
+
+}
+
+#endif
---
+++ src/Out.cpp
@@ -0,0 +1,86 @@
+#include "algs4/Out.h"
+
+#include <fstream>
+#include <ostream>
+#include <string>
+#include <cstdarg>
+#include <limits>
+#include <iomanip>
+
+namespace algs4 {
+
+Out::Out(std::ofstream &os) : out(os) {
+}
+
+Out::Out(std::ostream &os) : out(os) {
+}
+
+void Out::close() {
+}
+
+void Out::println() {
+    out << '\n';
+}
+
+void Out::println(const bool x) {
+    out << std::boolalpha << x << '\n';
+}
+
+void Out::println(const char x) {
+    out << x << '\n';
+}
+
+void Out::println(const double x) {
+    out << x << std::setprecision(std::numeric_limits<double>::digits10) << '\n';
+}
+
+void Out::println(const float x) {
+    out << x << std::setprecision(std::numeric_limits<double>::digits10) << '\n';
+}
+
+void Out::println(const int x) {
+    out << x << '\n';
+}
+
+void Out::println(const long x) {
+    out << x << '\n';
+}
+
+void Out::println(const byte x) {
+    // casting to int makes value of x printable
+    out << int(x) << '\n';
+}
+
+void Out::print() {
+    out << std::flush;
+}
+
+void Out::print(const bool x) {
+    out << std::boolalpha << x << std::flush;
+}
+
+void Out::print(const char x) {
+    out << x << std::flush;
+}
+
+void Out::print(const double x) {
+    out << x << std::setprecision(std::numeric_limits<double>::digits10) << std::flush;
+}
+
+void Out::print(const float x) {
+    out << x << std::setprecision(std::numeric_limits<float>::digits10) << std::flush;
+}
+
+void Out::print(const int x) {
+    out << x << std::flush;
+}
+
+void Out::print(const long x) {
+    out << x << std::flush;
+}
+
+void Out::print(const byte x) {
+    out << int(x) << std::flush;
+}
+
+}
