3476333c6a2398a689de1b7a6604ca0e98303216
vanilla milkshake <vanillamilkshakepls@gmail.com>
2021-12-02 03:35:39 +0300

    Added: example client for fast three sum algorithm

---
+++ demos/threesumfast_client.cpp
@@ -0,0 +1,12 @@
+#include "algs4/ThreeSumFast.h"
+#include "algs4/StdOut.h"
+#include "algs4/In.h"
+
+int main(int argc, const char *argv[]) {
+    using namespace algs4;
+    In in(argv[1]);
+
+    auto a = in.readAllInts();
+    int count = ThreeSumFast::count(a);
+    StdOut::println(count);
+}
