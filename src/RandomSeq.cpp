69f17077835fa8c6b19e0adcde986c146e654a75
vanilla milkshake <vanillamilkshakepls@gmail.com>
2021-12-06 10:03:40 +0300

    Added: RandomSeq program

---
+++ src/RandomSeq.cpp
@@ -0,0 +1,42 @@
+#include "algs4/StdRandom.h"
+#include "algs4/StdOut.h"
+
+#include <cstdlib>
+
+/*
+ * *  % java RandomSeq 5 100.0 200.0
+ *  123.43
+ *  153.13
+ *  144.38
+ *  155.18
+ *  104.02
+ */
+
+int main(int argc, const char *argv[]) {
+    using namespace algs4;
+
+    int n = std::atoi(argv[1]);
+
+    if (argc == 2) {
+        for (int i = 0; i < n; i++) {
+            double x = StdRandom::uniform();
+            StdOut::println(x);
+        }
+    }
+
+    else if (argc == 4) {
+        double lo = std::atof(argv[2]);
+        double hi = std::atof(argv[3]);
+
+        for (int i = 0; i < n; i++) {
+            double x = StdRandom::uniform(lo, hi);
+            StdOut::printf("%.2f\n", x);
+        }
+    }
+
+    else {
+        StdOut::println("Invalid number of arguments");
+    }
+
+    return 0;
+}
