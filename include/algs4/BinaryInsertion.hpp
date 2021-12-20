b2e794a2b72e258ef883479f45d3e1e22ee1122a
syntaxonly <vanilla.milkshake.for.life@gmail.com>
2021-12-21 00:46:07 +0300

    Ported: BinaryInsertion class

---
+++ include/algs4/BinaryInsertion.hpp
@@ -0,0 +1,54 @@
+#ifndef BINARYINSERTION_H
+#define BINARYINSERTION_H
+
+#include <algs4/StdOut.h>
+#include <vector>
+#include <cstdint>
+#include <cassert>
+
+namespace algs4 {
+namespace BinaryInsertion {
+
+template <typename T> bool isSorted(const std::vector<T> &a);
+
+template <typename T> void sort(std::vector<T> &a) {
+    const auto n = a.size();
+
+    for (std::size_t i = 1; i < n; i++) {
+        const auto v = a[i];
+        std::size_t lo = 0, hi = i;
+
+        while (lo < hi) {
+            const std::size_t mid = lo + (hi - lo) / 2;
+            if (v < a[mid]) hi = mid;
+            else lo = mid + 1;
+        }
+
+        for (std::size_t j = i; j > lo; j--)
+            a[j] = a[j - 1];
+        a[lo] = v;
+    }
+    assert(isSorted(a));
+}
+
+template <typename T>
+bool isSorted(const std::vector<T> &a, const std::size_t lo, const std::size_t hi) {
+    for (std::size_t i = lo + 1; i < hi; i++)
+        if (a[i] < a[i - 1]) return false;
+    return true;
+}
+
+template <typename T> bool isSorted(const std::vector<T> &a) {
+    return isSorted(a, 0, a.size());
+}
+
+template <typename T> void show(const std::vector<T> &a) {
+    for (std::size_t i = 0; i < a.size(); i++) {
+        StdOut::println(a[i]);
+    }
+}
+
+}
+}
+
+#endif
---
+++ tests/BinaryInsertion_test.cpp
@@ -0,0 +1,15 @@
+#include <algs4/BinaryInsertion.hpp>
+#include <algs4/StdIn.h>
+
+// BinaryInsertion_test < tiny.txt
+// BinaryInsertion_test < words3.txt
+
+int main(int argc, const char *argv[]) {
+    using namespace algs4;
+
+    auto a = StdIn::readAllStrings();
+    BinaryInsertion::sort(a);
+    BinaryInsertion::show(a);
+
+    return 0;
+}
